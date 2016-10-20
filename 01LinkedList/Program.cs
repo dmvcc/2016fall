7d58fe4ccf35a1e18a1a25ac51d1aa76eea188c1
Michael Dolan <Mike@Michaels-Air.home>
2016-10-20 19:52:24 -0400

    cs linked list commit

---
+++ 01LinkedList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist
+{
+    public class LinkedList<T>
+    {
+	    public Node<T> first;
+	    int count = 0;
+	
+	    public LinkedList<T>()
+	    {
+	      first = null;
+	    }
+
+	    public void Add(T input)
+	    {	
+		    Node newFirst = new Node<T>(input);
+		    newFirst.Next = first;
+		    first = newFirst;
+	    }
+	
+	    public void Delete(T toBeDeleted)
+	    {
+		    if( first.Value == toBeDeleted){
+		      first = first.Next;
+		      return;
+		    }
+        
+		    Node i,j;
+		    i = first;
+		    j = first.Next;
+				  
+		    while(j!=null && i!=null)
+		    {
+		      if(j.Value == toBeDeleted)
+		      {
+			    i.Next = j.Next;
+			    return;
+		      }
+		    }
+		    throw InvalidOperationException("No item was not found");
+	    }
+	
+	    public void Print()
+	    {
+		    Node i = first;
+		    while (i != null)
+		    {
+		      Console.WriteLine(i.Value + " ");
+		      i = i.Next;
+		    }
+	    }
+    }
+}
+
+*/
